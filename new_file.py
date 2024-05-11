9ed754277b84e3f47d1ff942efffee82e81263c1
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-05-11 17:30:19 +0200

    Add files via upload

--- new_file.py
+++ new_file.py
@@ -2,6 +2,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+from mininet.link import TCLink
 import time
 import threading
 import re
@@ -17,43 +18,34 @@ class LinearTopology(Topo):
         host1 = self.addHost('h1')
         host2 = self.addHost('h2')
 
-        # Adding links between the nodes
-        self.addLink(host1, switch1)
-        self.addLink(switch1, switch2)
-        self.addLink(switch2, host2)
+        # Adding links between the nodes with packet loss
+        self.addLink(host1, switch1, cls=TCLink, loss=5)  # 5% packet loss
+        self.addLink(switch1, switch2, cls=TCLink, loss=5)  # Additional link between switches also with packet loss
+        self.addLink(switch2, host2, cls=TCLink, loss=5)
 
-# Rest of the code...
-
-def run_iperf_flow(h1, h2_ip, server_port, duration, interval, results_file, bandwidth):
-    # Modify the bandwidth parameter in the iperf command
-    h1.popen(f'iperf -c {h2_ip} -p {server_port} -i {interval} -t {duration} -b {bandwidth}m -d > {result_file}', shell=True)
-
-    # Rest of the code...
+# Function to run iperf remains largely the same, we can modify it later if needed
 
 def create_linear_topology():
-    # Creating an instance of the linear topology defined at the start
     topo = LinearTopology()
 
-    # Starting the Mininet network
-    net = Mininet(topo)
+    # Starting the Mininet network with link TCLink for packet loss simulation
+    net = Mininet(topo, link=TCLink)
 
     # Starting the network
     net.start()
 
-    # Defining the number of parallel flows(this can be changed)
-    num_flows = 3
+    # Defining the number of parallel flows
+    num_flows = 5  # Changed from 1 to 5
 
-    # Opening results file
-    with open('five_flows_data.txt', 'a') as results_file:
+    # Opening a file in append mode to write the results
+    with open('Increase_in_BW', 'a') as results_file:
+        durations = [10]  # Durations over which iperf is run
+        intervals = [0.5]  # Intervals at which data is captured for each duration
+        num_steps = 150  # Number of steps (after each step, the bandwidth is increased by a factor of 10)
 
-        durations = [5, 10]  # durations over which iperf is run
-        intervals = [0.5, 0.5]  # intervals at which data is captured for each duration e.g at 0.5Sec for a duration of 10
-        num_runs = 1  # number or repetitions for which the iperf is run for each duration
-        bandwidths = [8, 2]  # bandwidths for each flow
-
-        for duration, interval, bandwidth in zip(durations, intervals, bandwidths):
-            for j in range(num_runs):
-                # Starting the iperf servers on host 2 for each flow
+        for duration, interval in zip(durations, intervals):
+            for step in range(1, num_steps + 1):
+                # Starting the iperf servers on host2 
                 servers = []
                 for flow_id in range(num_flows):
                     server_port = 5000 + flow_id
@@ -64,34 +56,33 @@ def create_linear_topology():
                 # Getting the IP address of h2
                 h2_ip = net.get('h2').IP()
 
-                # Starting the first flow with 80% of the link bandwidth
-                if flow_id == 0:
-                    thread = threading.Thread(target=run_iperf_flow, args=(net.get('h1'), h2_ip, server_port, duration, interval, results_file, bandwidth))
+                # Calculating dynamic bandwidth for this step
+                bandwidth = step * 10  # Increasing bandwidth by 10 Mbps for each step
+
+                # Creating and starting threads for each iperf flow
+                threads = []
+                for flow_id in range(num_flows):
+                    server_port = 5000 + flow_id
+                    thread = threading.Thread(target=run_iperf_flow, args=(net.get('h1'), h2_ip, server_port, duration, interval, results_file, f"{bandwidth}M"))
                     thread.start()
                     threads.append(thread)
 
-                # Starting the second flow with the remaining 20% of the link bandwidth
-                elif flow_id == 1:
-                    thread = threading.Thread(target=run_iperf_flow, args=(net.get('h1'), h2_ip, server_port, duration + 5, interval, results_file, bandwidth))
-                    thread.start()
-                    threads.append(thread)
-
-                # Waiting for the first flow to finish before starting the next iteration
-                if flow_id == 0:
-                    for thread in threads:
-                        thread.join()
+                # Waiting for all threads to finish
+                for thread in threads:
+                    thread.join()
 
                 # Stopping the iperf servers
                 for server in servers:
                     server.terminate()
 
-            results_file.write(f"End of {duration} seconds run\n")
-            results_file.write("-----\n")
+                results_file.write(f"End of step {step}, Bandwidth: {bandwidth} Mbps\n")
+                results_file.write("-----\n")
 
     # Opening the Mininet command line interface
     CLI(net)
     # Stopping the network once the CLI is closed
     net.stop()
+
 if __name__ == '__main__':
     setLogLevel('info')
     create_linear_topology()

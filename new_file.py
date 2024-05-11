7b30c2501123ac75228f2c66f692ab35c856ee02
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-05-11 18:13:50 +0200

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -76,13 +76,13 @@ def create_linear_topology():
     net.start()
 
     # Defining the number of parallel flows
-    num_flows = 3  # Changed from 1 to 5
+    num_flows = 2  # Changed from 1 to 5
 
     # Opening a file in append mode to write the results
     with open('Increase_in_BW', 'a') as results_file:
         durations = [10]  # Durations over which iperf is run
         intervals = [0.5]  # Intervals at which data is captured for each duration
-        num_steps = 150  # Number of steps (after each step, the bandwidth is increased by a factor of 10)
+        num_steps = 10  # Number of steps (after each step, the bandwidth is increased by a factor of 10)
 
         for duration, interval in zip(durations, intervals):
             for step in range(1, num_steps + 1):
